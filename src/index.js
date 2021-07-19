0eaf0f2fc764527eb9736c607025c3409ef2816e
EKjs <doomedarchnagel@gmail.com>
2021-07-19 13:52:28 +0200

    fixed imports

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { store } from './rdxstore/store';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
